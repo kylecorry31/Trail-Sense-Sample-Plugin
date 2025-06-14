27ac8e70c7b6c2f110cfc6cc488cdd9fe7c835ec
Kyle Corry <kylecorry31@gmail.com>
2025-06-14 17:32:33 -0400

    Update fragment

--- app/src/main/java/com/kylecorry/trail_sense_dem/ui/MainFragment.kt
+++ app/src/main/java/com/kylecorry/trail_sense_dem/ui/MainFragment.kt
@@ -10,15 +10,6 @@ import com.kylecorry.trail_sense_dem.infrastructure.DEM
 
 class MainFragment : XmlReactiveFragment(R.layout.fragment_main) {
     override fun onUpdate() {
-        val titleView = useView<Toolbar>(R.id.home_title)
-
-        val context = useAndroidContext()
-        val elevation = useBackgroundMemo {
-            DEM.getElevation(context, Coordinate(41.905, -71.695))
-        }
-
-        useEffect(titleView, elevation) {
-            titleView.title.text = elevation?.convertTo(DistanceUnits.Feet)?.distance?.toString()
-        }
+        // Do nothing
     }
 }
